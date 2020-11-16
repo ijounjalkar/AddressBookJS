248a686178dc7d4ca639326b3b136ac772ac6e0f
ijounjalkar <jounjalkarisha@gmail.com>
2020-11-16 16:27:18 +0530

    Added functionality to create contact with validating inputs.

--- AddressBook.js
+++ AddressBook.js
@@ -1,5 +1,10 @@
 //Usecase1:
 var prompt = require('prompt-sync')();
+let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+let addressRegex = RegExp('[A-z]{4,}$');
+let zipRegex = RegExp('[0-9]{3}[0-9]{3}');
+let phoneRegex = RegExp('[0-9]{2}[789]{1}[0-9]{2,10}');
+let emailRegex = RegExp('^(?=.*[A-Z])(?=.*[0-9])(?=.*[$#@!%_&])[A-Za-z0-9$#@!%_&]{8,}$');
 class Contact{
     constructor(...params){
         this.firstName = params[0];
@@ -10,6 +15,70 @@ class Contact{
         this.zip = params[5];
         this.phone = params[6];
         this.email = params[7];
+    } 
+    get firstName(){return this._firstName}
+    set firstName(firstName){
+        if(nameRegex.test(firstName)){
+            this._firstName = firstName;
+        }else{
+            throw "Invalid First Name";
+        }
+    }
+    get lastName(){return this._lastName}
+    set lastName(name){
+        if(nameRegex.test(name)){
+            this._lastName = name;
+        }else{
+            throw "Invalid Last Name";
+        }
+    }
+    get address(){return this._address}
+    set address(add){
+        if(addressRegex.test(add)){
+            this._address = add;
+        }else{
+            throw "Invalid address";
+        }
+    }
+    get city(){return this._city}
+    set city(cityName){
+        if(addressRegex.test(cityName)){
+            this._city = cityName;
+        }else{
+            throw "Invalid city";
+        }
+    }
+    get state(){return this._state};
+    set state(stateName){
+        if(addressRegex.test(stateName)){
+            this._state = stateName;
+        }else{
+            throw "Invalid state";
+        }
+    }
+    get zip(){return this._zip}
+    set zip(zipCode){
+        if(zipRegex.test(zipCode)){
+            this._zip = zipCode;
+        }else{
+            throw "Invalid zip";
+        }
+    }
+    get phone(){return this._phone}
+    set phone(number){
+        if(phoneRegex.test(number)){
+            this._phone = number;
+        }else{
+            throw "Invalid phone number";
+        }
+    } 
+    get email(){return this._email}
+    set email(email){
+        if(emailRegex.test(emailRegex)){
+            this._email = email;
+        }else{
+            throw "Invalid Email";
+        }
     }  
     toString(){
         return "FirstName: "+this.firstName+" LastName: "+this.lastName + " Address: "+this.address + " City: "+this.city + " State: "+this.state 
